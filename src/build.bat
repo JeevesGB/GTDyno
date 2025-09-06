3a8d690fe5860149d217c3a3c7a1448b64900adc
JeevesGB <jeeveslord@gmail.com>
2025-09-06 23:47:19 +0100

    bat

--- src/build.bat
+++ src/build.bat
@@ -2,7 +2,7 @@
 set EXENAME=GTDyno
 
 REM Build gtd.py into a single EXE with no console window
-py -m pyinstaller --onefile --windowed --name %EXENAME% gtd.py
+pyinstaller --onefile --windowed --name %EXENAME% gtd.py
 
 echo.
 echo %EXENAME% built!
